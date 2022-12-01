0e29df68890a8c3cce21e046475812dae974205f
ZhjMr <2744764939@qq.com>
2022-12-01 11:51:02 +0800

    animate 字体图标以及公共样式的引入

--- App.vue
+++ App.vue
@@ -14,4 +14,11 @@
 
 <style>
 	/*每个页面公共css */
+	@import url("common/free.css");
+	/* 主题色样式表 */
+	@import url("common/common.css");
+	/* animate动画引入 */
+	@import url("common/animate.min.css");
+	/* 引入字体图标 */
+	@import url("common/iconfont.css");
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,17 +1,18 @@
 <template>
 	<view>
-		
+		<text class="iconfont icon-search"></text>
+		<h1 class="animate__animated animate__bounce">An animated element</h1>
 	</view>
 </template>
 <script>
-	export default{
-		data(){
-			return{
-				
+	export default {
+		data() {
+			return {
+
 			}
 		}
 	}
 </script>
 <style lang="scss">
-	
+
 </style>
